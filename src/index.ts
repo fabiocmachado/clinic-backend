6ca64d5730ed1416860c484b85f6009939330841
Fabio Machado <camposmachado.fabio@gmail.com>
2024-06-12 10:16:19 +0100

    DB connect works

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,19 @@
 import express from "express";
-//import { Pool } from "pg";
 import dotenv from "dotenv";
-import { constants } from "buffer";
-import { send } from "process";
+import pool from "./db";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT;
 
+pool
+  .connect()
+  .then(() => console.log("PostgreSQL connected"))
+  .catch((err) => console.error("Connection error", err.stack));
+
 app.get("/", (req, res) => {
-  res.send("Hello world");
+  res.send("Bem-vindo à sua aplicação de gerenciamento de paciente!");
 });
 
 app.listen(PORT, () => {
